39518a784f55a6a57d5473e0ec07906ed757cbe2
Soeren Etler <soeren.etler@gmail.com>
2019-08-03 12:31:14 +0200

    new version Sa 3. Aug 12:31:14 CEST 2019

--- _pages/about.md
+++
@@ -1,8 +0,0 @@
----
-permalink: /about/
-title: About
----
-
-I am Sören, a master student at the University of Potsdam in the field of Cognitive Systems. I am fascinated by Machine Learning (I am still trying to get my head around Deep Learning), Natural Language Processing and everything that involves data (especially visualising data).
-
-You can find me on [LinkedIn](https://www.linkedin.com/in/s%C3%B6ren-etler-434a6ba7/)
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "My first Blogpost",
+        "excerpt":"I am Sören, a master student at the University of Potsdam in the field of Cognitive Systems. I am fascinated by Machine Learning (I am still trying to get my head around Deep Learning), Natural Language Processing and everything that involves data (especially visualising data). I want to start an...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2019/08/02/my-first-blogpost.html",
+        "teaser":null}]
